3758219213a1b0db70e8758b776b8dca9c9d6bb7
Elena <lama_elena@mail.ru>
2021-01-13 21:03:53 +0300

    Задание №2

--- src/main/java/ru/elena/lesson_1/MainClass.java
+++ src/main/java/ru/elena/lesson_1/MainClass.java
@@ -2,6 +2,14 @@ package ru.elena.lesson_1;
 
 public class MainClass {
     public static void main(String[] args) {
-        
+        // Это тигр
+        byte age = 15;
+        short tailLength = 60;
+        int clawsQty = 30;
+        long dailyKkal = 5600L;
+        float height = 1.07F;
+        double price = 5345056.59;
+        char gender = 'F';
+        boolean resident = true;
     }
 }
