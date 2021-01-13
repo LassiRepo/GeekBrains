76d88a556c8817731055196ff3b97edbfe20e69b
Elena <lama_elena@mail.ru>
2021-01-13 22:38:55 +0300

    Задание №3

--- src/main/java/ru/elena/lesson_1/MainClass.java
+++ src/main/java/ru/elena/lesson_1/MainClass.java
@@ -2,6 +2,19 @@ package ru.elena.lesson_1;
 
 public class MainClass {
     public static void main(String[] args) {
-        
+        float z = 9.2F;
+        float x = 56.20F;
+        float m = -22F;
+        float v = 633F;
+
+        float result = calculate(z, x, m, v);
+        System.out.println(result);
+
     }
+
+    public static  float calculate(float a, float b, float c, float d){
+        float calculate = a * (b + (c / d));
+        return calculate;
+    }
+
 }
