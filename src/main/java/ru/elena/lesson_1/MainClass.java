7897251ea8a5b20aef2a86c921130e9474d5329e
Elena <lama_elena@mail.ru>
2021-01-14 23:02:03 +0300

    Задание №7

--- src/main/java/ru/elena/lesson_1/MainClass.java
+++ src/main/java/ru/elena/lesson_1/MainClass.java
@@ -1,7 +1,13 @@
 package ru.elena.lesson_1;
-
+//7. Написать метод, которому в качестве параметра передается строка, обозначающая имя.
+// Метод должен вывести в консоль сообщение «Привет, указанное_имя!».
 public class MainClass {
     public static void main(String[] args) {
-        
+        String name = "Елена";
+        printName(name);
+    }
+
+    public static void printName(String name){
+        System.out.println("Привет, " + name + "!");
     }
 }
