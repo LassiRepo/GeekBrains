3d83ab0b690ee3f351bf3241c51c451786f415a2
Elena <lama_elena@mail.ru>
2021-01-14 22:40:20 +0300

    Задание №6

--- src/main/java/ru/elena/lesson_1/MainClass.java
+++ src/main/java/ru/elena/lesson_1/MainClass.java
@@ -1,7 +1,19 @@
 package ru.elena.lesson_1;
 
+//6. Написать метод, которому в качестве параметра передается целое число. Метод должен вернуть true, если число
+// отрицательное, и вернуть false если положительное.
 public class MainClass {
     public static void main(String[] args) {
-        
+        int a = -10;
+        checkValue(a);
+        System.out.println(checkValue(a));
+    }
+    
+    public static boolean checkValue(int a) {
+        if (a < 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
