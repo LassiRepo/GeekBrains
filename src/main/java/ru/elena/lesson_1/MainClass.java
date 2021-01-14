a5c8c483facf873b7047f3abc19fe0704d3b5379
Elena <lama_elena@mail.ru>
2021-01-14 21:22:38 +0300

    Задание №5

--- src/main/java/ru/elena/lesson_1/MainClass.java
+++ src/main/java/ru/elena/lesson_1/MainClass.java
@@ -1,7 +1,21 @@
 package ru.elena.lesson_1;
 
+//5. Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль, положительное ли
+//   число передали или отрицательное. Замечание: ноль считаем положительным числом.
 public class MainClass {
     public static void main(String[] args) {
-        
+        int number = 50;
+        checkNumber(number);
+
     }
+    public static void checkNumber(int number) {
+        if (number >= 0) {
+            System.out.println("Number is positive");
+        }
+        else {
+            System.out.println("Number is negative");
+        }
+
+    }
+
 }
